e43c272582b08cb47095165d215e86865990ce69
Austin Chen <austin.yhc@gmail.com>
2020-08-14 18:37:17 +0800

    Interface Copyable

--- tutorial/cherno/cherno_memory_track.cc
+++ tutorial/cherno/cherno_memory_track.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 
+// I will create a dirty simple way to track the memory usage
+// by overloading the new and delete operator.
 struct AllocationMetrices {
     uint32_t TotalAllocated = 0;
     uint32_t TotalFreed = 0;
@@ -21,19 +23,26 @@ void operator delete(void* memory, size_t size) {
     free(memory);
 }
 
-struct Object {
+struct SensorReadings {
     int x, y, z;
 };
 
 int main() {
     PrintMemoryUsage();
-    std::string str = "Cherno";
+    // Interesting: seems like there is a scenario that the std::string
+    // won't allocate memory on the heap. BTW, avoid to use any string
+    // in the embedded world unless it is neccessary.
+    //
+    // Caveat here, using standard libraries are not generally expected to
+    // be extensive or widespread in the embedded environemens where this
+    // code will run.
+    std::string sensorName = "Simulated Piezo Force Sensor";
     PrintMemoryUsage();
 
-    Object* obj = new Object;
+    SensorReadings* reading = new SensorReadings;
     PrintMemoryUsage();
     {
-        std::unique_ptr<Object> obj_uptr = std::make_unique<Object>();
+        std::unique_ptr<SensorReadings> p_reading = std::make_unique<SensorReadings>();
         PrintMemoryUsage();
     }
     PrintMemoryUsage();
